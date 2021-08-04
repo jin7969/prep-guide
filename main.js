382223c91d23802e99dbd98e9ca3896146662c48
Ahn <82227098+jin7969@users.noreply.github.com>
2021-08-05 02:07:10 +0900

    bg changer

---
+++ main.js
@@ -0,0 +1,40 @@
+const click = document.querySelector(".click");
+
+function hexGenerator() {
+  const hexNumbers = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F'
+  ];
+
+  let result = "#";
+
+  for (var i = 0; i < 6; i += 1) {
+    const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    result += hexNumbers[randomIndex];
+  }
+
+  changeColor(result);
+}
+
+function changeColor(color) {
+  const name = document.querySelector(".colorName");
+
+  document.body.style.background = color;
+  name.innerText = `HEX COLOR : ${color}`
+}
+
+click.addEventListener("click", hexGenerator);
